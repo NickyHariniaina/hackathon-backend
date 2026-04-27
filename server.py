cfca2a13f5461f53ae4d18e1f271e7d1a1400e31
colombefioren <colomberakotonjanahary@gmail.com>
2026-04-27 23:51:07 +0300

    fix: remove percentage

--- server.py
+++ server.py
@@ -33,4 +33,4 @@ def read_root():
     return {"Hello": "World"}
 
 if __name__ == "__main__":
-    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=False)%
+    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=False)
