a1e60aa7bfe6e4de0243edacbdea9ea558c44dca
NickyHariniai <ferrissushi@gmail.com>
2026-04-27 23:54:44 +0300

    chore: remove some endpoint

--- server.py
+++ server.py
@@ -28,9 +28,5 @@ app.include_router(codeAgent_router, prefix="/api", tags=["Code Agent"])
 app.include_router(chatAgent_router, prefix="/api", tags=["Chat Agent"])
 app.include_router(crudAgent_router, prefix="/api", tags=["CRUD Agent"])
 
-@app.get("/api")
-def read_root():
-    return {"Hello": "World"}
-
 if __name__ == "__main__":
     uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=False)
